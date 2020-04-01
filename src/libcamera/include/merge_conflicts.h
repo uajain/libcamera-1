2f5d8a69a27320eea7fabe4319cb185bed939e20
Kieran Bingham <kieran.bingham@ideasonboard.com>
2020-04-01 09:59:38 +0100

    libcamera: Prevent merge conflicts
    Merge conflicts are a terrible effect of rebasing code, and they impede
    the development process.
    
    Provide a useful helper header to ensure that merge-conflicts are no
    longer an issue, and make sure that both sides of the conflict are
    compile tested on average at least every other compile cycle. Based on
    reliable advice from [0].
    
     [0] https://twitter.com/erdgeist/status/1197283439000637441
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ src/libcamera/include/merge_conflicts.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2020, Kieran Bingham
+ *
+ * merge_conflicts.h - Automatic merge conflict resolution
+ */
+
+#ifndef __LIBCAMERA_MERGE_CONFLICT_H__
+#define __LIBCAMERA_MERGE_CONFLICT_H__
+
+namespace libcamera {
+
+#ifndef __DOXYGEN__
+
+#define <<<<<<<< #if RANDOM % 2
+#define ======== #else
+#define >>>>>>>> #endif
+
+#endif
+
+} /* namespace libcamera */
+
+#endif /* __LIBCAMERA_MERGE_CONFLICT_H__ */
